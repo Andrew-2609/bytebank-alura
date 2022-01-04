7eaa84c6e56eb3b03c5a1c7b359db2a570148f5b
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 08:54:24 -0300

    Feat: add implementation of relationship between Conta and Cliente in TestaBanco.java

--- src/compound/TestaBanco.java
+++ src/compound/TestaBanco.java
@@ -1,4 +1,19 @@
 package compound;
 
+import models.Cliente;
+import models.Conta;
+
 public class TestaBanco {
+    public static void main(String[] args) {
+        Cliente primeiroCliente = new Cliente();
+        primeiroCliente.nome = "Andrew Monteiro";
+        primeiroCliente.cpf = "909.909.909-09";
+        primeiroCliente.profissao = "Programador";
+
+        Conta conta = new Conta();
+        conta.depositar(2000.0);
+        conta.titular = primeiroCliente;
+
+        System.out.println("Nome do titular da conta: " + conta.titular.nome);
+    }
 }
