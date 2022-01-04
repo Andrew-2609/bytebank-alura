499e933446d911a0855de9b60a9b76db996e8deb
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 10:43:22 -0300

    Feat: add implementation of TestaValores using constructors and static attribute

--- src/encapsulated/TestaValores.java
+++ src/encapsulated/TestaValores.java
@@ -1,7 +1,14 @@
 package encapsulated;
 
+import models.Conta;
+
 public class TestaValores {
     public static void main(String[] args) {
+        Conta primeiraConta = new Conta(113, 238664);
+        Conta segundaConta = new Conta(113, 238665);
+        System.out.println("O número da primeira conta é: " + primeiraConta.getNumero());
+        System.out.println("A agência da segunda conta é: " + segundaConta.getAgencia());
 
+        System.out.println("O total de contas cadastradas é: " + Conta.getTotal());
     }
 }
