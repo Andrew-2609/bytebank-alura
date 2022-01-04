0ba29c342c0b0ce5ed99494f7c8f31b0ce6c4020
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 07:46:23 -0300

    Feat: add new Conta to main method of CriaConta

--- src/core/CriaConta.java
+++ src/core/CriaConta.java
@@ -1,4 +1,9 @@
 package core;
 
 public class CriaConta {
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta();
+        primeiraConta.saldo = 200;
+        System.out.println(primeiraConta.saldo);
+    }
 }
