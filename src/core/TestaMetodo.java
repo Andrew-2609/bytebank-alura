eb53c406b7e70767f178b5407a1013f786b81411
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 08:29:15 -0300

    Feat: add sacar() method call in TestaMetodo

--- src/core/TestaMetodo.java
+++ src/core/TestaMetodo.java
@@ -3,8 +3,14 @@ package core;
 public class TestaMetodo {
     public static void main(String[] args) {
         Conta conta = new Conta();
+
         conta.saldo = 500;
         conta.depositar(50);
         System.out.println(conta.saldo);
+
+        boolean conseguiuSacar = conta.sacar(200);
+        if (conseguiuSacar) {
+            System.out.println(conta.saldo);
+        }
     }
 }
