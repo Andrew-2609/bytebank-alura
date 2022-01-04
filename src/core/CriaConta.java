4ec2d2bf598f46b2f0e77270b7ad80d48256c714
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 08:06:01 -0300

    Feat: add new object of Conta

--- src/core/CriaConta.java
+++ src/core/CriaConta.java
@@ -4,6 +4,10 @@ public class CriaConta {
     public static void main(String[] args) {
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 200;
-        System.out.println(primeiraConta.saldo);
+        System.out.println("Primeira conta tem: " + primeiraConta.saldo);
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 50;
+        System.out.println("Segunda conta tem: " + segundaConta.saldo);
     }
 }
