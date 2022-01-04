0245dac2a62117a45f3ce151e7dd544842b15cc0
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 08:17:01 -0300

    Feat: add implementation of TesteReferencias.java

--- src/core/TesteReferencias.java
+++ src/core/TesteReferencias.java
@@ -1,4 +1,25 @@
 package core;
 
 public class TesteReferencias {
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta();
+        Conta segundaConta = primeiraConta;
+        Conta terceiraConta = new Conta();
+
+        if (primeiraConta == segundaConta) {
+            System.out.println("A primeira e a segunda conta são iguais, pois apontam para o mesmo objeto.");
+        } else {
+            System.out.println("A primeira e a segunda conta não são iguais, são objetos diferentes.");
+        }
+
+        if (primeiraConta != terceiraConta && segundaConta != terceiraConta) {
+            System.out.println("A primeira e a segunda contas são diferentes da terceira, pois ela aponta para um outro objeto.");
+        } else {
+            System.out.println("Todas as contas são iguais.");
+        }
+
+        System.out.println("primeiraConta:\t" + primeiraConta);
+        System.out.println("segundaConta:\t" + segundaConta);
+        System.out.println("terceiraConta:\t" + terceiraConta);
+    }
 }
