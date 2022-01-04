95898bc4bc575e14c4bdfb0b8b55c5b7d4e174df
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 09:00:17 -0300

    Feat: add implementation of TesteContaSemCliente

--- src/compound/TesteContaSemCliente.java
+++ src/compound/TesteContaSemCliente.java
@@ -1,4 +1,19 @@
 package compound;
 
+import models.Cliente;
+import models.Conta;
+
 public class TesteContaSemCliente {
+    public static void main(String[] args) {
+        Conta contaDoArthur = new Conta();
+
+        System.out.println("O nome do titular da nova conta por enquanto é: " + contaDoArthur.titular);
+
+        contaDoArthur.titular = new Cliente();
+        contaDoArthur.titular.nome = "Arthur";
+
+        contaDoArthur.depositar(1000.0);
+
+        System.out.println("O titular da conta se chama: " + contaDoArthur.titular.nome + " e possui saldo de: " + contaDoArthur.saldo);
+    }
 }
