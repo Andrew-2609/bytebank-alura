ece856e96efe16fa98c87b89f323dacdf9a9480c
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 08:28:55 -0300

    Feat: create method sacar()

--- src/core/Conta.java
+++ src/core/Conta.java
@@ -9,4 +9,12 @@ public class Conta {
     public void depositar(double valor) {
         this.saldo += valor;
     }
+
+    public boolean sacar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
 }
