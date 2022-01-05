212caae3fa219bf954d3489c06addd988a39a61c
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-05 09:13:14 -0300

    Feat: add basic implementation of CalculadorDeImposto.java

--- src/models/CalculadorDeImposto.java
+++ src/models/CalculadorDeImposto.java
@@ -1,4 +1,15 @@
 package models;
 
 public class CalculadorDeImposto {
+
+    private double totalImposto;
+
+    public void registrar(Tributavel tributavel) {
+        this.totalImposto += tributavel.getValorImposto();
+    }
+
+    public double getTotalImposto() {
+        return this.totalImposto;
+    }
+
 }
