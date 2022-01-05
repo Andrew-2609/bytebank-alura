7fa44145178e7f3f3542663d671eac224d33c0f8
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-05 09:14:37 -0300

    Feat: add very basic implementation of SeguroDeVida with Tributavel interface implemented

--- src/models/SeguroDeVida.java
+++ src/models/SeguroDeVida.java
@@ -1,4 +1,10 @@
 package models;
 
-public class SeguroDeVida {
+public class SeguroDeVida implements Tributavel {
+
+    @Override
+    public double getValorImposto() {
+        return 42;
+    }
+
 }
