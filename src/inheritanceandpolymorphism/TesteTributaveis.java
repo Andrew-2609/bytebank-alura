568408197068b8e959809fbc811058250dd210b9
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-05 09:18:17 -0300

    Feat: implement class TesteTributaveis using classes that implement Tributavel interface

--- src/inheritanceandpolymorphism/TesteTributaveis.java
+++ src/inheritanceandpolymorphism/TesteTributaveis.java
@@ -1,4 +1,20 @@
 package inheritanceandpolymorphism;
 
+import models.CalculadorDeImposto;
+import models.ContaCorrente;
+import models.SeguroDeVida;
+
 public class TesteTributaveis {
+    public static void main(String[] args) {
+        ContaCorrente contaCorrente = new ContaCorrente(231, 928);
+        contaCorrente.depositar(100.0);
+
+        SeguroDeVida seguroDeVida = new SeguroDeVida();
+
+        CalculadorDeImposto calculadorDeImposto = new CalculadorDeImposto();
+        calculadorDeImposto.registrar(contaCorrente);
+        calculadorDeImposto.registrar(seguroDeVida);
+
+        System.out.println("Total de impostos: " + calculadorDeImposto.getTotalImposto());
+    }
 }
