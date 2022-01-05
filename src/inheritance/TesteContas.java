8d9fb382c15892673cf708249e878de0a3fce1a4
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 21:48:35 -0300

    Feat: add basic implementation of TesteContas to transfer money between ContaCorrente and ContaPoupanca subclasses

--- src/inheritance/TesteContas.java
+++ src/inheritance/TesteContas.java
@@ -1,4 +1,19 @@
 package inheritance;
 
+import models.ContaCorrente;
+import models.ContaPoupanca;
+
 public class TesteContas {
+    public static void main(String[] args) {
+        ContaCorrente cc = new ContaCorrente(111, 111);
+        cc.depositar(200.0);
+
+        ContaPoupanca cp = new ContaPoupanca(222, 222);
+        cp.depositar(800.0);
+
+        cc.transferir(10.0, cp);
+
+        System.out.println("Saldo da conta corrente: " + cc.getSaldo());
+        System.out.println("Saldo ca conta poupança: " + cp.getSaldo());
+    }
 }
