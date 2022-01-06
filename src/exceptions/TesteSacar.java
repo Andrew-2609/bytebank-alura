251aede83919a4aff8b4b9325f6a3f533aacf442
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-06 07:25:19 -0300

    Feat: add implementation of TesteSacar throwing a SaldoInsuficienteException in the main() method

--- src/exceptions/TesteSacar.java
+++ src/exceptions/TesteSacar.java
@@ -1,4 +1,12 @@
 package exceptions;
 
+import models.ContaCorrente;
+
 public class TesteSacar {
+    public static void main(String[] args) {
+        ContaCorrente cc = new ContaCorrente(123, 321);
+        cc.depositar(200.0);
+        cc.sacar(210.0);
+        System.out.println(cc.getSaldo());
+    }
 }
