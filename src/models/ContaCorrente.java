9fe23b8d39c842c8133f48c0628926b753e07f6a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-07 05:41:21 -0300

    Feat: override method toString() in ContaCorrente.java

--- src/models/ContaCorrente.java
+++ src/models/ContaCorrente.java
@@ -19,4 +19,9 @@ public class ContaCorrente extends Conta implements Tributavel {
         return super.getSaldo() * 0.01;
     }
 
+    @Override
+    public String toString() {
+        return "toString:ContaCorrente";
+    }
+
 }
