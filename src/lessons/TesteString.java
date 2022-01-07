781da294d7f22e431d16423566f63d091864928d
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-07 05:46:45 -0300

    Feat: add basic implementation of TesteString.java

--- src/lessons/TesteString.java
+++ src/lessons/TesteString.java
@@ -1,4 +1,11 @@
 package lessons;
 
+import models.ContaCorrente;
+
 public class TesteString {
+    public static void main(String[] args) {
+        ContaCorrente contaCorrente = new ContaCorrente(123, 432);
+
+        System.out.println(contaCorrente);
+    }
 }
