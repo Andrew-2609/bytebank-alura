12e4ce2729ed2d06f319e47a05b4d99879ef1fad
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-07 05:49:17 -0300

    Feat: add implementation of ContaPoupanca in TesteString.java

--- src/lessons/TesteString.java
+++ src/lessons/TesteString.java
@@ -1,11 +1,16 @@
 package lessons;
 
 import models.ContaCorrente;
+import models.ContaPoupanca;
 
 public class TesteString {
     public static void main(String[] args) {
         ContaCorrente contaCorrente = new ContaCorrente(123, 432);
+        contaCorrente.depositar(1000.0);
+        ContaPoupanca contaPoupanca = new ContaPoupanca(890, 987);
+        contaPoupanca.depositar(3000.0);
 
         System.out.println(contaCorrente);
+        System.out.println(contaPoupanca);
     }
 }
