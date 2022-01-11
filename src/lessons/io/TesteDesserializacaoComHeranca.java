f59dbc6e38a035ed914aeb1cf6d032a0fc55791b
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-10 22:00:21 -0300

    Feat: add base implementation of TesteDesserializacaoComHeranca

--- src/lessons/io/TesteDesserializacaoComHeranca.java
+++ src/lessons/io/TesteDesserializacaoComHeranca.java
@@ -1,5 +1,19 @@
 package lessons.io;
 
-public class TesteDesserializacaoComHeranca {
+import models.ContaCorrente;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class TesteDesserializacaoComHeranca {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("cc.bin"));
+        ContaCorrente contaCorrente = (ContaCorrente) objectInputStream.readObject();
+        objectInputStream.close();
+
+        System.out.println(contaCorrente);
+
+    }
 }
