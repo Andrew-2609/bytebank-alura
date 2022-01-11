a2c8f0c54240dfbccc4e8f07251916493ff8ff09
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-10 21:50:12 -0300

    Feat: add a serial version UID to Cliente

--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -3,6 +3,8 @@ package models;
 import java.io.Serializable;
 
 public class Cliente implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String nome;
     private String cpf;
     private String profissao;
