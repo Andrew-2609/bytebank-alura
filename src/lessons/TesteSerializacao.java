0ecf759c5b2042c02b68266db778477489614f3c
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-10 21:42:29 -0300

    Feat: add basic implementation of TesteSerializacao

--- src/lessons/TesteSerializacao.java
+++ src/lessons/TesteSerializacao.java
@@ -1,4 +1,22 @@
 package lessons;
 
+import models.Cliente;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class TesteSerializacao {
+    public static void main(String[] args) throws IOException {
+
+        Cliente cliente = new Cliente();
+        cliente.setNome("Andrew Monteiro");
+        cliente.setCpf("000.000.000-00");
+        cliente.setProfissao("Programador");
+
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("cliente.bin"));
+        objectOutputStream.writeObject(cliente);
+        objectOutputStream.close();
+
+    }
 }
