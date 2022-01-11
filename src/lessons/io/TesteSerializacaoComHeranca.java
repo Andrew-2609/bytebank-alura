2d0aeeb26028e6c75f41db584c1baaf8b047236b
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-10 21:56:06 -0300

    Feat: add basic implementation of TesteSerializacaoComHeranca

--- src/lessons/io/TesteSerializacaoComHeranca.java
+++ src/lessons/io/TesteSerializacaoComHeranca.java
@@ -1,5 +1,27 @@
 package lessons.io;
 
-public class TesteSerializacaoComHeranca {
+import models.Cliente;
+import models.ContaCorrente;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class TesteSerializacaoComHeranca {
+    public static void main(String[] args) throws IOException {
+
+        Cliente cliente = new Cliente();
+        cliente.setNome("Jordan Rivers");
+        cliente.setCpf("000.000.000-01");
+        cliente.setProfissao("Fisher");
+
+        ContaCorrente contaCorrente = new ContaCorrente(0, 1);
+        contaCorrente.setTitular(cliente);
+        contaCorrente.depositar(222.3);
+
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("cc.bin"));
+        objectOutputStream.writeObject(cliente);
+        objectOutputStream.close();
+
+    }
 }
